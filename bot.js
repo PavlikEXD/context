a1213295b60ff66eade1d65b9a0507632c7baa98
pavel-kovylnikov <pavel.kovylnikov@jetrockets.com>
2023-05-24 13:38:06 +0400

    Added db

--- bot.js
+++ bot.js
@@ -3,10 +3,69 @@ const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
 const tmi = require('tmi.js');
+const { Pool } = require('pg');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
+
+const dbPool = new Pool({
+  connectionString: 'postgresql://localhost/twitch_development'
+});
+
+async function createMessagesTable() {
+  try {
+    const dbClient = await dbPool.connect();
+    const result = await dbClient.query(`
+      SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = 'messages')
+    `);
+    const exists = result.rows[0].exists;
+
+    if (!exists) {
+      await dbClient.query(`
+        CREATE TABLE messages (
+          id SERIAL PRIMARY KEY,
+          user_name VARCHAR(255),
+          message TEXT
+        )
+      `);
+      console.log('Table "messages" created');
+    } else {
+      console.log('Table "messages" already exists');
+    }
+
+    dbClient.release();
+  } catch (error) {
+    console.error('Error creating table "messages":', error);
+  }
+}
+
+async function createDatabase() {
+  try {
+    const dbClient = new Pool({
+      connectionString: 'postgresql://localhost/postgres'
+    });
+
+    const result = await dbClient.query(`
+      SELECT COUNT(*) FROM pg_database WHERE datname = 'twitch_development'
+    `);
+    const exists = result.rows[0].count === '1';
+
+    if (!exists) {
+      await dbClient.query('CREATE DATABASE twitch_development');
+      console.log('Database "twitch_development" created');
+    } else {
+      console.log('Database "twitch_development" already exists');
+    }
+
+    dbClient.end();
+  } catch (error) {
+    console.error('Error creating database "twitch_development":', error);
+  }
+}
+
+createDatabase();
+createMessagesTable();
 
 const opts = {
   identity: {
@@ -22,20 +81,41 @@ client.on('message', onMessageHandler);
 client.on('connected', onConnectedHandler);
 client.connect();
 
-function onMessageHandler(target, context, msg, self) {
+async function onMessageHandler(target, context, msg, self) {
   if (self) {
     return;
   }
 
   const commandName = msg.trim();
 
+  // Проверить, что сообщение состоит из одного слова и этого слова нет в базе данных
+  // Если условие выполняется, то сохранить сообщение в базу данных
+  if (commandName.split(' ').length === 1) {
+    const result = await dbPool.query('SELECT * FROM messages WHERE message = $1', [commandName]);
+    if (result.rows.length === 0) {
+      saveMessageToDB(context.username, commandName);
+    }
+  }
+
   if (commandName === '!dice') {
     const num = rollDice();
     client.say(target, `${context.username} rolled a ${num}`);
-    handleDiceCommand(num);
+    handleDiceCommand(context.username, num);
     console.log(`* Executed ${commandName} command`);
   } else {
     console.log(`* Unknown command ${commandName}`);
+  }
+}
+
+async function saveMessageToDB(username, message) {
+  try {
+    await dbPool.query('INSERT INTO messages (user_name, message) VALUES ($1, $2)', [
+      username,
+      message
+    ]);
+    console.log(`* Saved chat message: ${username}: ${message}`);
+  } catch (error) {
+    console.error('Error saving chat message:', error);
   }
 }
 
@@ -43,8 +123,13 @@ function onConnectedHandler(addr, port) {
   console.log(`* Connected to ${addr}:${port}`);
 }
 
-function handleDiceCommand(num) {
-  io.emit('diceRoll', num);
+async function handleDiceCommand(username, num) {
+  try {
+    io.emit('diceRoll', num);
+    console.log(`* Emulated dice roll: ${username} rolled a ${num}`);
+  } catch (error) {
+    console.error('Error handling dice command:', error);
+  }
 }
 
 function rollDice() {
